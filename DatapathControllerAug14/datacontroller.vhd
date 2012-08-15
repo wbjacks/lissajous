e424e32b85274d45071c9387b933a04062fca9c6
pyukman <pyukman@dartmouth.edu>
2012-08-15 00:14:39 -0400

    Should now have beginning datapath controller

---
+++ DatapathControllerAug14/datacontroller.vhd
@@ -0,0 +1,84 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity datacontroller is
+	port( clk50		: in  std_logic;
+			serialD0	: in  std_logic;
+			serialD1	: in  std_logic;
+			inewclk	: in	std_logic;
+			ia2dcdiv	: in	integer;
+			iCS		: out std_logic;
+			idata0	: out	unsigned(11 downto 0);
+			idata1	: out	unsigned(11 downto 0)
+		);
+end datacontroller ; 
+
+architecture behavioral of datacontroller  is
+	type state_type is (idle,readD0,readD1);
+	signal state 		: state_type := readD0;
+	signal dCount		: integer := 0;
+	
+begin
+	state_logic: process(clk50)
+		begin
+			if rising_edge(clk50) then
+				
+				case state is
+					
+					when idle =>
+						iCS <= '1';
+						if ((ia2dcdiv = 5) and (inewclk = '1')) then
+							if (dCount = 15) then
+								dCount <= 0;
+								state <= readD0;
+							else
+								dCount <= dCount + 1;
+								state <= idle;
+							end if;
+						else
+							state <= idle;
+						end if;
+						
+					when readD0 =>
+						iCS <= '0';
+						if ((ia2dcdiv = 5) and (inewclk = '1')) then
+							if (dCount < 4) then
+								dCount <= dCount + 1;
+								state <= readD0;
+							elsif ((dCount > 3) and (dCount < 16)) then
+								dCount <= dCount + 1;
+								idata0(15 - dCount) <= serialD0;
+								state <= readD0;
+							else
+								dCount <= 0;
+								state <= readD1;
+							end if;
+						else
+							state <= readD0;
+						end if;
+							
+					when readD1 =>
+						iCS <= '1';
+						if ((ia2dcdiv = 5) and (inewclk = '1')) then
+							if (dCount < 4) then
+								dCount <= dCount + 1;
+								state <= readD1;
+							elsif ((dCount > 3) and (dCount < 16)) then
+								dCount <= dCount + 1;
+								idata1(15 - dCount) <= serialD1;
+								state <= readD1;
+							else
+								dCount <= 0;
+								state <= idle;
+							end if;
+						else
+							state <= readD1;
+						end if;
+							
+				end case;
+			end if;
+		end process state_logic;
+
+end behavioral;
---
+++ DatapathControllerAug14/toplevel.vhd
@@ -0,0 +1,61 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity toplevel is
+	port( clk50		: in  std_logic;
+			D0			: in  std_logic;
+			D1			: in 	std_logic;
+			CS			: out std_logic;
+			Sclk		: out std_logic
+		);
+end toplevel; 
+
+architecture structural of toplevel is
+	
+	component datacontroller is
+		port( clk50 	: IN std_logic;
+				serialD0 : IN std_logic;
+				serialD1 : IN std_logic;
+				inewclk	: IN std_logic;
+				ia2dcdiv	: IN integer;
+				iCS 		: OUT std_logic;
+				idata0	: OUT	unsigned(11 downto 0);
+				idata1	: OUT unsigned(11 downto 0)
+			);
+	end component;
+
+	signal a2dclkdiv	: integer := 0;
+	signal data0		: unsigned(11 downto 0) := "000000000000";
+	signal data1		: unsigned(11 downto 0) := "000000000000";
+	signal newclk		: std_logic := '0';
+	
+begin
+	
+	Sclk <= newclk;
+
+	U0: datacontroller
+	 port map( 	clk50		=> clk50,
+					serialD0 => D0,
+					serialD1 => D1,
+					inewclk	=> newclk,
+					ia2dcdiv	=> a2dclkdiv,
+					iCS 		=> CS,
+					idata0	=>	data0,
+					idata1	=> data1
+				);
+	
+	a2dclockdiv: process(clk50)
+		begin
+			if rising_edge(clk50) then
+				if (a2dclkdiv = 5) then
+					newclk <= not(newclk);
+					a2dclkdiv <= 0;
+				else
+					a2dclkdiv <= a2dclkdiv + 1;
+				end if;
+			end if;
+		end process a2dclockdiv;
+	
+end structural;
